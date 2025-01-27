c713f2f873e03dbb722df0a2b6024d02e72b351a
Ben Collins <bcollins@egnyte.com>
2025-01-26 20:57:01 -0500

    update the codebase with new breadcrumb block and corrected installation procedures

--- modules/cavalla_nodes/cavalla_page/src/Entity/CavallaPage.php
+++ modules/cavalla_nodes/cavalla_page/src/Entity/CavallaPage.php
@@ -8,14 +8,12 @@
  * @package  cavalla_page
  * @author   Ben Collins <bcolli31@gmail.com>
  * @license  GNU General Public License, version 2 or later
- * @link     https://www.example.com
+ * @link https://www.example.com
  */
 
 namespace Drupal\cavalla_page\Entity;
 
 use Drupal\cavalla\CavallaBase;
-use Drupal\cavalla\Entity\Cavalla;
-use Drupal\paragraphs\Entity\Paragraph;
 
 /**
  * Defines the CavallaPage class.
@@ -24,26 +22,18 @@ use Drupal\paragraphs\Entity\Paragraph;
  */
 class CavallaPage extends CavallaBase {
 
-    /**
-     * Retrieves content from the Paragraph Bundle.
-     *
-     * @param array $entity
-     *   The Paragraph Bundle entities.
-     *
-     * @return array
-     *   An associative array containing block content from the Paragraph Bundle.
-     */
-    public function getValue(array $entity) {
-        $arr = [];
-
-        foreach ($entity as $key => $paragraph) {
-            // Load the paragraph entity.
-            $site_blocks = Paragraph::load($paragraph['target_id']);
-
-         //   dump($site_blocks);
-        }
-
-        return $arr;
-    }
+  /**
+   * Retrieves content from the Paragraph Bundle.
+   *
+   * @param array $entity
+   *   The Paragraph Bundle entities.
+   *
+   * @return array
+   *   An associative array containing block content from the Paragraph Bundle.
+   */
+  public function getValue(array $entity) {
+    $arr = [];
+    return $arr;
+  }
 
 }
